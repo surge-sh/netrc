a2f0dc2f7101286d60bb5dc3c4ded38becaa526c
Kenneth Ormandy <kenneth@chloi.io>
2015-09-03 17:24:36 -0700

    Removes use of _netrc

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ module.exports = exports = function(file) {
   var home = getHomePath();
 
   if (!file && !home) return {};
-  file = file || join(home, (/^win/.test(process.platform)?"_netrc":".netrc"));
+  file = file || join(home, ".netrc");
 
   if (!file || !fs.existsSync(file)) return {};
   var netrc = fs.readFileSync(file, 'UTF-8');
